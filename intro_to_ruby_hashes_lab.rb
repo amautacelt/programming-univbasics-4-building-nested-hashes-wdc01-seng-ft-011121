b480eb1ac4e7344898f421ce6739a16f0ea385df
C. L <amautacelt@gmail.com>
2020-12-27 05:41:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,12 @@ def monopoly_with_third_tier
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
       },
-      :names => {}
+      :names => {
+        :reading_railroad =>  {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
     }
   }
 
@@ -50,5 +55,6 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
 
 end
