0254fb5a830b7ccf6f89303385124e9dc5d2332b
C. L <amautacelt@gmail.com>
2020-12-27 05:30:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,11 @@ def monopoly_with_second_tier
   # complexity.
   
   hash = {
-    :railroads => {:pieces => 4}
+    :railroads => {
+      :pieces => 4}, 
+      :rent_in_dollars => {}, 
+      :names => {}
+    }
   }
 
 end
